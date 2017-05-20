87c40bf6d9105ac92a535e8f525e493af3f6fd4d
kor45cw <kor45cw@gmail.com>
2017-05-20 14:59:11 +0900

    Finish While.ml First Make

---
+++ hw3/parser.mli
@@ -0,0 +1,36 @@
+type token =
+  | NUM of (int)
+  | ID of (string)
+  | INT
+  | PLUS
+  | MINUS
+  | STAR
+  | SLASH
+  | EQUAL
+  | EQUALEQUAL
+  | LE
+  | LT
+  | GE
+  | GT
+  | NOT
+  | AND
+  | OR
+  | IF
+  | ELSE
+  | WHILE
+  | DO
+  | READ
+  | PRINT
+  | SEMICOLON
+  | LBRACE
+  | RBRACE
+  | LBLOCK
+  | RBLOCK
+  | LPAREN
+  | RPAREN
+  | EOF
+  | TRUE
+  | FALSE
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> While.program
